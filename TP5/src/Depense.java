4aae624b8de45e8ab72fe04f2ba267e888fdaac4
Yannis-barache <ybarache@icloud.com>
2023-03-15 16:59:45 +0100

    Debut ajout de doc

--- TP5/src/Depense.java
+++ TP5/src/Depense.java
@@ -3,21 +3,36 @@ public class Depense{
     private String produit;
     private Personne payeur;
 
+    /** Créer une dépense
+     * @param personne Une personne 
+     * @param montant Un double qui représente le prix du produit
+     * @param produit Un String qui décrit ce qu'est le produit
+     */
     public Depense(Personne personne, double montant, String produit){
         this.payeur=personne;
         this.montant=montant;
         this.produit=produit;
     }
 
+    /** Retourne le nom du produit
+     * @return le nom du produit
+     */
+
     public String getProduit(){
         return this.produit;
     }
 
 
+    /** Retourne le prix du produit
+     * @return le prix du produit
+     */
     public double getMontant(){
         return this.montant;
     }
 
+    /** Retourne la personne ayant acheté le produit
+     * @return La personne
+     */
     public Personne getPersonne(){
         return this.payeur;
     }
--- TP5/src/WeekEnd.java
+++ TP5/src/WeekEnd.java
@@ -8,20 +8,34 @@ public class WeekEnd {
     private List<Personne> participants;
 
 
+    /** Créer une instance de WeekEnd à partir d'une date
+     * @param String La date du weekEnd
+     */
     public WeekEnd(String date){
         this.dateDuWeekEnd=date;
         this.lesDepenses= new ArrayList<>();
         this.participants= new ArrayList<>();
     }
 
+    /** Ajoute un participant à la liste des participants du WE
+     * @param prenom Un string représentant le nom du participant
+     * @param age Un int représentant l'âge de la personne
+     */
     public void ajouteParticipant(String prenom, int age){
         this.participants.add(new Personne(prenom, age));
     }
 
+    /** Ajoute un participant à la liste des participants du WE
+     * @param personne La personne à ajouter
+     */
     public void ajouteParticipant(Personne personne){
         this.participants.add(personne);
     }
 
+    /** Ajoute une dépense au WE
+     * @param montant Un double qui représente le prix du produit
+     * @param produit Un string 
+     */
     public void ajouteDepense(double montant, String produit, Personne personne){
         this.lesDepenses.add(new Depense(personne, montant, produit));
     }
