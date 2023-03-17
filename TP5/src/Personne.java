13d1fcfcafe7b18a547473796d79d522d878b128
Yannis-barache <112858061+Yannis-barache@users.noreply.github.com>
2023-03-17 13:28:20 +0100

    Fin de la doc

--- TP5/src/Personne.java
+++ TP5/src/Personne.java
@@ -2,11 +2,19 @@ public class Personne {
     private String prenom;
     private int age;
 
+    /** Créer une personne
+     * @param prenom Un String qui représente le prénom de la personne
+     * @param age Un int qui représente l'âge de la personne
+     */
+
     public Personne(String prenom,int age){
         this.prenom=prenom;
         this.age=age;
     }
 
+    /** Retourne le prénom de la personne
+     * @return le prénom de la personne
+     */
     public String getNom(){
         return this.prenom;
     }
--- TP5/src/WeekEnd.java
+++ TP5/src/WeekEnd.java
@@ -9,7 +9,7 @@ public class WeekEnd {
 
 
     /** Créer une instance de WeekEnd à partir d'une date
-     * @param String La date du weekEnd
+     * @param date La date du weekEnd
      */
     public WeekEnd(String date){
         this.dateDuWeekEnd=date;
@@ -40,6 +40,10 @@ public class WeekEnd {
         this.lesDepenses.add(new Depense(personne, montant, produit));
     }
 
+    /** Retourne le total des dépenses d'une personne
+     * @param personne La personne dont on veut le total des dépenses
+     * @return Son total des dépenses
+     */
     public double totalDepense(Personne personne){
         double somme=0;
         for (Depense depense:this.lesDepenses){
@@ -50,6 +54,10 @@ public class WeekEnd {
         return somme;
     }
 
+    /** Retourne le total des dépenses du WE
+     * @return Le total des dépenses du WE
+     */
+
     public double totalDepense(){
         double somme=0;
         for (Depense depense:this.lesDepenses){
@@ -58,6 +66,10 @@ public class WeekEnd {
         return somme;
     }
 
+    /** Retourne le total des dépenses pour un produit
+     * @param produit Le produit dont on veut le total des dépenses
+     * @return Le total des dépenses pour le produit
+     */
     public double totalDepense(String produit){
         double somme=0;
         for (Depense depense:this.lesDepenses){
@@ -69,6 +81,10 @@ public class WeekEnd {
         
     }
 
+    /** Retourne ce que la personne doit au autre
+     * @param personne La personne dont on veut l'avoir
+     * @return Ce qu'elle doit ou ce que lui doit
+     */
     public double avoirPersonne(Personne personne){
         if (this.lesDepenses.size()==0){
             return 0.0;
